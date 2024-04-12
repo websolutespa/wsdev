6cf9433b39bce9ca4d94a72fd8ec4550fdc1982c
Luca De Blasio <deblasioluca@gmail.com>
2024-04-12 11:38:22 +0200

    add base theme

--- samples/wordpress/src/js/main.js
+++ samples/wordpress/src/js/main.js
@@ -2,18 +2,26 @@ import 'virtual:accessible';
 import 'virtual:spritemap';
 import 'virtual:theme.css';
 import '../css/main.scss';
+//
+import { colorScheme } from './common/colorScheme';
+import { lazyLoad } from './common/lazyLoad';
+import { lenis } from './common/lenis';
 
-export default class Main {
-  constructor() {
-    this.bind();
-  }
+addEventListener('DOMContentLoaded', () => {
+  colorScheme();
+  lazyLoad();
+  lenis();
+});
 
-  bind() {}
+import icons from 'virtual:icons';
+import theme from 'virtual:theme';
+
+if (import.meta.env.DEV) {
+  console.info('%cws-vite ' + import.meta.env.WS_VITE, 'color: #757575');
+  // console.log('env', import.meta.env);
+  console.log('icons', icons);
+  console.log('theme', theme);
 }
-
-window.onload = () => {
-  new Main();
-};
 
 function threeshake() {
   console.log(
